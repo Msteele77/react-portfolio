53e8466f79fb9238adbc8eea6459740d2f870f7c
Macheya Steele <mcheysteele@gmail.com>
2021-04-06 15:24:35 -0600

    resume and contact components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 //import Project from './components/Project';
 import About from './components/About';
+import ContactForm from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
+
+
+  
+
   return (
     <div>
       <main>
         <Header></Header>
 
         <About></About>
+        <ContactForm></ContactForm>
+        <Resume></Resume>
         <Footer></Footer>
     </main>
   </div>
---
+++ src/components/Resume/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Resume () {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <p>Download My Full Resume Here:</p>
+                <h4>Front-End Proficiencies</h4>
+
+
+                <h4>Back-End Proficiencies</h4>
+
+
+
+        </section>
+
+
+
+    )
+}
+
+export default Resume;
