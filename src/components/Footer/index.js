030f4ea7932675bedac8121a224bd9cdef9cd14e
Macheya Steele <mcheysteele@gmail.com>
2021-04-06 13:56:25 -0600

    added img

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,27 @@ import React from 'react';
 
 function Footer() {
   return (
-    <section>
-      <h2 id="footer">I'm a footer</h2>
-    </section>
+    <div className="flex-row">
+                <div className="contacts">
+                    <ul>
+                        <li>
+                           <a href="mailto:mcheysteele@gmail.com">
+                             <img src="src/assets/images/email.png" alt="email-icon"></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Msteele77">
+                              <img src="github.png" alt="github-icon"></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/macheya-steele">
+                              <img src={require("linkedin.png")} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
   );
 }
 
