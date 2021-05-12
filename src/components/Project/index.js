bfad7f355bd4d4e971cb3842168408b826989a6b
Macheya Steele <mcheysteele@gmail.com>
2021-05-12 10:45:19 -0600

    added project layout and styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,30 +1,69 @@
 import React from 'react';
 import budgetTracker from "../../assets/images/budget.png";
 
-function Project (props) {
-  
+function Project(props) {
+
   const projects = {
-    name:"Budget Tracker",
+    name: "Budget Tracker",
     description: "Budget Tracker Description"
   };
 
   return (
-    
-      <section>
-            
 
+    <section>
 
-            
-            <div className="flex-row">
-              <img
-                src={budgetTracker}
-                className="img-thumbnail mx-1"
-                />
-            </div>
+      <article id="projects" class="content-section">
 
-               
-      </section>
-            
+        <div class="flex-column">
+         
+        </div>
+
+        <div class="grid-wrapper">
+          <div class="grid-container-2">
+
+            <a class="grid-item item-1" href="https://msteele77.github.io/run-buddy/">
+              <div class="project-heading">
+                <p>Run Buddy</p>
+              </div>
+            </a>
+
+            <a class="grid-item item-2" href="https://msteele77.github.io/horiseon/">
+              <div class="project-heading">
+                <p>Horiseon</p>
+              </div>
+            </a>
+
+            <a class="grid-item item-3" href="https://minimal-prepper.herokuapp.com/">
+              <div class="project-heading">
+                <p>The Minimal Prepper
+                     </p>
+              </div>
+            </a>
+
+            <a class="grid-item item-4" href="https://msteele77.github.io/Eats-n-such/">
+              <div class="project-heading">
+                <p>Project 4</p>
+              </div>
+            </a>
+
+            <a class="grid-item item-5" href="https://www.google.com/">
+              <div class="project-heading">
+                <p>Project 5</p>
+              </div>
+            </a>
+
+            <a class="grid-item item-6" href="https://msteele77.github.io/run-buddy/">
+              <div class="project-heading">
+                <p>Run Buddy</p>
+              </div>
+            </a>
+
+          </div>
+        </div>
+      </article>
+
+    </section>
+
   );
 }
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,13 +4,38 @@ function Resume () {
     return (
         <section id="resume">
             <h2>Resume</h2>
-            <p>Download My Full Resume Here:</p>
-                <h4>Front-End Proficiencies</h4>
-
-
-                <h4>Back-End Proficiencies</h4>
-
-
+            <a href="https://docs.google.com/document/d/1taCjdFslpVA41B9quC5gUc_RPEnrvCeH/edit#">Click me to download my full resume!</a>
+                
+                <ul>
+                <h4>Proficiencies</h4>
+                    <li>HTML / CSS</li>
+                    <li>Git / Github / Heroku</li>
+                    <li>Bootstrap</li>
+                    <li>DOM</li>
+                    <li>APIs</li>
+                    <li>JQuery</li>
+                    <li>JSON</li>
+                    <li>AJAX</li>
+                    <li>Node</li>
+                    <li>ES6</li>
+                    <li>Object-Oriented Programming</li>
+                    <li>Object Relational Mapping</li>
+                    <li>Handlebars</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>MVC Paradigm</li>
+                    <li>Sequelize</li>
+                    <li>Testing</li>
+                    <li>Agile Development</li>
+                    <li>Progressive Web Apps</li>
+                    <li>React</li>
+                    <li>NoSQL</li>
+                    <li>MERN Stack</li>
+                    <li>Mongo DB</li>
+                    <li>Model View Controller</li>
+                </ul>
+                
+                
 
         </section>
 
